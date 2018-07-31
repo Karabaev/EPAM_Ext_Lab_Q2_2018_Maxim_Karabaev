b15be3a794448a1578ac32e498bca63c2a106521
Karabaev Maxim <maxkarab@yandex.ru>
2018-07-31 16:27:21 +0400

    Added Task8

---
+++ Task8/Northwind/Scripts/Task8_1/Task 8_1_1.sql
@@ -0,0 +1,5 @@
+﻿SELECT OrderID, ShippedDate, ShipVia
+FROM Northwind.Orders
+--WHERE ShippedDate > DATEFROMPARTS(1998, 5, 6) AND ShipVia >= 2
+WHERE ShippedDate > '19960506' AND ShipVia >= 2
+--По заданию сказано искать после 02.05.1998 но таких записей нет
---
+++ Task8/Northwind/Scripts/Task8_1/Task 8_1_3.sql
@@ -0,0 +1,7 @@
+﻿SELECT OrderID as 'Order Number',
+CASE
+	WHEN ShippedDate IS NULL THEN 'Not shipped' ELSE CONVERT(nchar, ShippedDate) 
+END as 'Shipped date'
+FROM Northwind.Orders
+WHERE ShippedDate >'19960506' OR ShippedDate IS NULL
+--Выводится ли дата в формате по умолчанию?
